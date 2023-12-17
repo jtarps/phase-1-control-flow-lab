065093ae618b4e8d76a9ae02d4393655d5222f4b
Jaydee <jaydee.tarpeh@gmail.com>
2023-12-17 16:01:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+   let result;
+   if (ride <= 400) {
+    result = 'This one is on me!'
+   } else if (ride > 400 && ride <= 2000 ){
+    result = 'Pay me my money'
+   } else if (ride > 2000 && ride <= 2500) {
+    result = 'I will gladly take your thirty bucks.'
+   } else {
+    result = 'No can do.'
+   }
+
+   return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet(1567))
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity('Toronto'));
+
+
+function switchOnCharmFromTip(tipAmount){
+
+  switch (tipAmount){
+  case 'generous' : 
+    return 'Thank you so much.';
+
+  case 'not as generous' : 
+    return 'Thank you.';
+
+  default: 
+    return 'Bye.';
+  }
 }
+
+console.log(switchOnCharmFromTip('Generous'))
